b60f39fabe1556e47ca0506662faab5cdaea26eb
Theo <theocarr777@gmail.com>
2015-05-13 00:38:38 +1200

    Set up a chime/sound puzzle. Still need to add sounds.

---
+++ Assets/ChimePuzzleController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChimePuzzleController : MonoBehaviour {
+
+	public GameObject[] chimes; 
+	private int currentOrder = 1;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	private void randomizeChimeOrder(){
+		int numOfChimes = chimes.Length;
+		int chimeOrder = 1;
+
+		//for (int i =numOfChimes; i<numOfChimes; i++) {
+			//if chime hasn't been selected
+			//increaes order
+			//choose a random chime
+		//continue until all chimes are given a number
+		while(chimeOrder<chimes.Length){	
+		int chosenChime = Mathf.RoundToInt(Random.Range (0,chimes.Length));
+			//if it hasnt been set, set it
+			if(chimes[chosenChime].GetComponent<ChimeScript>().setOrder(chimeOrder)){
+				chimeOrder++;
+			}
+		}
+
+	}
+
+	public int getCurrentOrder(){
+		return currentOrder;
+	}
+
+	public void increaseCurrentOrder(){
+		currentOrder++;
+		if (currentOrder >= chimes.Length) {
+			puzzleComplete ();
+		}
+	}
+
+	private void puzzleComplete(){
+	//give a key or unlock some room
+	}
+}
---
+++ Assets/ChimeScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChimeScript : MonoBehaviour {
+
+	public int order;
+	public GameObject chimePuzzleController;
+
+	// Use this for initialization
+	void Start () {
+		order = -1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public bool activate(){
+		if (chimePuzzleController.GetComponent<ChimePuzzleController> ().getCurrentOrder () == order) {
+			print("correct order" +order);
+			chimePuzzleController.GetComponent<ChimePuzzleController> ().increaseCurrentOrder ();
+			return true;
+		}
+		return false;
+	}
+
+	public bool setOrder(int ord){
+		if (order == -1) {
+			order = ord;
+			return true;
+		}
+		return false;
+	}
+}
