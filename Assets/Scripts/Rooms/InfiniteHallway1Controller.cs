7f22a81cbaaec023c9391d36081de1b335fc14d1
Theo <theocarr777@gmail.com>
2015-05-15 02:08:36 +1200

    Started making infinite hallway puzzle/room. Need to adjust positions.

---
+++ Assets/Scripts/Rooms/InfiniteHallway1Controller.cs
@@ -0,0 +1,42 @@
+﻿
+using System.Collections;
+using UnityEngine;
+using Escape.Core;
+using Escape.Util;
+
+namespace Escape.Rooms
+{
+	public class InfiniteHallway1Controller : BaseRoomController
+	{
+		
+		// Use this for initialization
+		void Start ()
+		{
+			//always loads next infinite hallway
+			doors [0].SetWeight ("InfiniteHallway2", 1);
+		}
+		
+		// Update is called once per frame
+		void Update ()
+		{
+			
+		}
+		
+		override public Transform GetEntrance ()
+		{
+			return base.entrance;
+		}
+		
+		override public void Shuffle() 
+		{
+			base.Shuffle ();
+			Logging.Log ("(InfiniteHallway) shuffled!");
+		}
+		
+		
+		public override void ItemPickedUp(string name)
+		{
+			
+		}
+	}
+}
