ea7bda1a9b7c7381c8cf320500fb2c1813ac246a
Paul Mathews <pfcmathews@gmail.com>
2015-06-17 19:59:11 +1200

    Trying to fix a bug in the painting puzzle that happens when it is re loaded at a different angle

--- Assets/Scripts/WallPaintingPosition.cs
+++ Assets/Scripts/WallPaintingPosition.cs
@@ -27,16 +27,17 @@ public class WallPaintingPosition : InteractableObject {
 	public void Throw() {
 		if (currentPainting != null) {
 			Rigidbody rigid = currentPainting.GetComponent<Rigidbody> ();
+			//currentPainting.transform.Translate(Vector3.forward * 0.5f);
 			rigid.isKinematic = false;
-			currentPainting.transform.Translate(currentPainting.transform.TransformDirection(new Vector3(0.5f,0f,0f)));
-			rigid.AddForce (this.transform.forward * Random.Range(50f,100f));
+			rigid.AddForce (rigid.transform.forward * Random.Range(50f,100f));
 		}
 	}
 
 	public override void Interact(PickupableObject with=null) {
 		if (with == null && currentPainting != null) { // then the player should pick up the current painting
 			// pop it off the wall first
-			currentPainting.transform.Translate(currentPainting.transform.TransformVector(new Vector3(0.1f,0.0f,0.0f)));
+			//currentPainting.transform.Translate(currentPainting.transform.TransformVector(new Vector3(0.1f,0.0f,0.0f)));
+			Throw ();
 			PlayerStatus.GiveObjectToHold (currentPainting);
 			currentPainting = null;
 		} else if (with != null && currentPainting == null) {
