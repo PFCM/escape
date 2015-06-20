08042d5d056fa6a25a07336c6472f92e9d824948
Theo <theocarr777@gmail.com>
2015-06-20 20:48:54 +1200

    added a main menu

---
+++ Assets/MainMenuController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.Characters.FirstPerson;
+using Escape.Core;
+using Escape.Util;
+using UnityEngine.UI;
+
+public class MainMenuController : MonoBehaviour {
+
+	public Button exitButton;
+	public Button startButton;
+
+	// Use this for initialization
+	void Start () {
+		exitButton.onClick.AddListener(() => { Application.Quit (); });
+		startButton.onClick.AddListener(() => { Application.LoadLevel ("EntranceHall"); });
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
