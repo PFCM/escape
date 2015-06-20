95caac976ab2a53c2c29c46d557300fba06c012f
Theo <theocarr777@gmail.com>
2015-06-20 17:05:08 +1200

    entrancehall out of secret room now positioned properly

--- Assets/Scripts/MonsterStanding.cs
+++ Assets/Scripts/MonsterStanding.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class MonsterStanding : MonoBehaviour {
 	
-	public int timer = 200;
+	public int timer = 150;
 	
 	// Use this for initialization
 	void Start () {
