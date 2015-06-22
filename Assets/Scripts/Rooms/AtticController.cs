5bdcace2a659a2ffd63755ba59789fedc40e0445
Theo <theocarr777@gmail.com>
2015-06-22 17:05:48 +1200

    set up for attic

---
+++ Assets/Scripts/Rooms/AtticController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using Escape.Core;
+using Escape.Util;
+
+namespace Escape.Rooms
+{
+	public class AtticController : BaseRoomController
+	{
+		
+		// Use this for initialization
+		void Start ()
+		{
+			doors [0].SetWeight ("Nursery", 1);
+			doors [0].SetWeight ("LivingRoom", 1);
+			doors [0].SetWeight ("HallStairwayDown", 1);
+		}
+		
+		// Update is called once per frame
+		void Update ()
+		{
+			
+		}
+		
+		public override Transform GetEntrance() 
+		{
+			return transform;
+		}
+		
+		public override void ItemPickedUp(string name)
+		{
+			// do something
+			Logging.Log ("(Nursery) Player picked up " + name);
+		}
+		
+		public override void Shuffle() // TODO: move this into super class, even just as a helper
+		{
+			base.Shuffle ();
+			foreach (PositionableObject obj in objects) {
+				obj.ChooseRandomPositionUniform ();
+			}
+		}
+	}
+}
