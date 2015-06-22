01323e1fa4e2f55dc77e2a3d6f4eed4c1f88a54a
Theo <theocarr777@gmail.com>
2015-06-23 11:12:38 +1200

    set up for cell and endGameTrigger

---
+++ Assets/Scripts/Rooms/CellController.cs
@@ -0,0 +1,41 @@
+﻿
+using System.Collections;
+using UnityEngine;
+using Escape.Core;
+using Escape.Util;
+
+namespace Escape.Rooms
+{
+	public class CellController : BaseRoomController
+	{
+		
+		// Use this for initialization
+		void Start ()
+		{
+
+		}
+		
+		// Update is called once per frame
+		void Update ()
+		{
+			
+		}
+		
+		override public Transform GetEntrance ()
+		{
+			return base.entrance;
+		}
+		
+		override public void Shuffle() 
+		{
+			base.Shuffle ();
+			Logging.Log ("(Cell) shuffled!");
+		}
+		
+		
+		public override void ItemPickedUp(string name)
+		{
+			
+		}
+	}
+}
