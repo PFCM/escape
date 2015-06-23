a7830fb9e33ba7007b9ffa552b44e1a2309995e2
Paul Mathews <pfcmathews@gmail.com>
2015-06-23 20:04:35 +1200

    Added some of Levi's footsteps

--- Assets/Scripts/FirstPersonController.cs
+++ Assets/Scripts/FirstPersonController.cs
@@ -180,7 +180,7 @@ namespace UnityStandardAssets.Characters.FirstPerson
 			// excluding sound at index 0
 			int n = Random.Range(1, m_FootstepSounds.Length);
 			m_AudioSource.clip = m_FootstepSounds[n];
-			m_AudioSource.PlayOneShot(m_AudioSource.clip);
+			m_AudioSource.PlayOneShot(m_AudioSource.clip, 0.75f);
 			// move picked sound to index 0 so it's not picked next time
 			m_FootstepSounds[n] = m_FootstepSounds[0];
 			m_FootstepSounds[0] = m_AudioSource.clip;
